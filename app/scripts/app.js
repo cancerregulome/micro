2190242a7fedd585aa882b3f9fa6bbeb757e100e
Dick Kreisberg <rkreisberg@systemsbiology.org>
2013-05-02 18:04:53 -0700

    layout addition scatterplots when data is clicked

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,27 +14,98 @@ function ($, _, vq, queue, URL, scatterplot) {
         "dataset" : "gbm_2013_pub_tumor_only"
     };
 
-  var Application = {
-  
-    initialize: function() {
-        var url = new URL(window.location);
-        _.extend(options, url.params);
+function populateScatterplots(data) {
+    var gene = data["GEXP"],
+        rppa = data["RPPA"],
+        mirn = data["MIRN"];
 
-        queue()
-        .defer(d3.json, "data/gbm-pub2013.json")
-        .await( function (err, points) {
-            drawScatterplot(points);
+    $.when( feature_request(gene,"GEXP"), feature_request(rppa.split(':')[0], "RPPA", rppa.split(':')[1]), feature_request(mirn,"MIRN"))
+            .done( function(g_data, r_data, m_data ) {
+                drawFeatureScatterplot(JSON.parse(g_data[0])[0],JSON.parse(r_data[0])[0]);
+                drawFeatureScatterplot(JSON.parse(m_data[0])[0],JSON.parse(r_data[0])[0]);
+            });
+
+  }
+
+  function feature_request (label, type, modifier) {
+    return $.ajax("/google-dsapi-svc/addama/datasources/re/gbm_2013_pub_tumor_only_features/query?tq="
+         + encodeURIComponent('select alias, patient_values where `label` = \'' + 
+                label + '\' and source = \'' +  type + '\'' +
+                (_.isUndefined(modifier) ? '' : ' and label_desc = \'' + modifier + '\' ')
+                + ' limit 1')
+          + "&tqx=" + encodeURIComponent('out:json_array'));
+  }
+
+  function drawFeatureScatterplot(x_axis,rppa) {
+        var type = x_axis.alias.split(':')[1],
+        rppa_values = rppa.patient_values.split(":").map(parseFloat),
+        x_values = x_axis.patient_values.split(":").map(parseFloat);
+
+        var data_array = _.map(rppa_values, function(val, index) { 
+            var obj = {"RPPA" : val};
+            obj[type] = x_values[index]; 
+            return obj;
         });
 
-        function drawScatterplot (data_array) {
-               var sp =  new vq.ScatterPlot();
+        $('#' + type.toLowerCase() + "_scatterplot").html("");
+
+        var x_fields = x_axis.alias.split(':'),
+        x_label = x_fields[1] + ' ' + x_fields[2],
+        rppa_fields = rppa.alias.split(':'),
+        rppa_label = rppa_fields[2] +' ' + rppa_fields[7];
+
+        data_array = data_array.filter(function(obj) { return _.isFinite(obj["RPPA"] + obj[type]);});
+
+         var feature_sp =  new vq.ScatterPlot();  
+         var plot_data = {
+                    DATATYPE : "vq.models.ScatterPlotData",
+                    CONTENTS : {
+                        PLOT : {
+                            container: document.getElementById(type.toLowerCase() + "_scatterplot"),
+                            width : 380, height: 240,
+                            dblclick_notifier : function() {},
+                            vertical_padding : 80,
+                            horizontal_padding: 100,
+                            x_label_displacement: 40,
+                            y_label_displacement: -70,
+                            x_tick_displacement: 20,
+                            enable_transitions: true
+                        },
+                        axis_font :"20px helvetica",
+                        tick_font :"20px helvetica",
+                        stroke_width: 2,
+                        radius: 3,
+                        data_array: data_array,
+                        regression: 'none',
+                        ycolumnid: type,
+                        xcolumnid: 'RPPA',
+                        valuecolumnid: 'id',
+                        tooltip_items : {
+                           
+                         },
+                        tooltip_timeout : 200,
+                        ycolumnlabel: x_label,
+                        xcolumnlabel: rppa_label,
+                        brush_handler: function(d) {
+                            console.log(d);
+                        },
+                        click_handler: function(d) {
+                        },
+                    }
+                };
+                feature_sp.draw(plot_data);
+                return feature_sp;
+        }
+
+  function drawCorrelationScatterplot (data_array) {
+               var corr_sp =  new vq.ScatterPlot();
 
                 var plot_data = {
                     DATATYPE : "vq.models.ScatterPlotData",
                     CONTENTS : {
                         PLOT : {
-                            container: document.getElementById("scatterplot"),
-                            width : 824, height: 624,
+                            container: document.getElementById("corr_scatterplot"),
+                            width : 600, height: 450,
                             dblclick_notifier : function() {},
                             vertical_padding : 80,
                             horizontal_padding: 100,
@@ -52,25 +123,41 @@ function ($, _, vq, queue, URL, scatterplot) {
                         ycolumnid: 'gexp_corr',
                         xcolumnid: 'mirn_corr',
                         valuecolumnid: 'id',
+                        tooltip_items : {
+                            Gene : 'GEXP',
+                            Protein : 'RPPA',
+                            uRNA : 'MIRN',
+                            'Gene Corr' : 'gexp_corr',
+                            'uRNA Corr' : 'mirn_corr'
+                         },
+                        tooltip_timeout : 200,
                         ycolumnlabel: 'GEXP<->RPPA Correlation',
                         xcolumnlabel: 'uRNA<->RPPA Correlation',
                         brush_handler: function(d) {
                             console.log(d);
-                        }
+                        },
+                        click_handler: function(d) {
+                            populateScatterplots(d);
+                        },
                     }
                 };
-
-                $("#btn-toggle-brush").on("click", function() {
-                    sp.enableBrush();
-                });
-
-                $("#btn-toggle-zoom").on("click", function() {
-                    sp.enableZoom();
-                });
-
-                sp.draw(plot_data);
-                return sp;
+                corr_sp.draw(plot_data);
+                return corr_sp;
         }
+
+  var Application = {
+  
+    initialize: function() {
+        var url = new URL(window.location);
+        _.extend(options, url.params);
+
+        queue()
+        .defer(d3.json, "data/gbm-pub2013.json")
+        .await( function (err, points) {
+            drawCorrelationScatterplot(points);
+        });
+
+        
 
     }
 
