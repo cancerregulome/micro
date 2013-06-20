6cb103213a1c903296bf16a404b189fbe02de10c
Dick Kreisberg <rkreisberg@systemsbiology.org>
2013-06-19 17:25:14 -0700

    added color legend and improved dynamic color scale

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -130,7 +130,7 @@ function populateScatterplots(data) {
                                 color_property : 'mirn_gexp_corr',
                                 xcolumn_id : 'mirn_corr',
                                 xcolumnlabel: 'miRNA <-> RPPA Correlation'
-                                });
+                                }).legend('#color_legend');
             } else if (value === 'GEXP') {
                     correlationScatterPlot({
                                 data_array: data_points,
@@ -138,7 +138,7 @@ function populateScatterplots(data) {
                                 color_property : 'mirn_corr',
                                 xcolumn_id : 'mirn_gexp_corr',
                                 xcolumnlabel: 'miRNA <-> GEXP Correlation'
-                                });
+                                }).legend('#color_legend');
             }
         });
     }
@@ -165,7 +165,7 @@ function populateScatterplots(data) {
                 color_property : 'mirn_gexp_corr',
                 xcolumn_id : 'mirn_corr',
                 xcolumnlabel: 'miRNA <-> RPPA Correlation'
-            });
+            }).legend('#color_legend');
             $('#highlight').autocomplete({source: _.pluck(data_points,'GEXP')});
             data_points = points;
         });
--- app/scripts/correlation_scatterplot.js
+++ app/scripts/correlation_scatterplot.js
@@ -18,16 +18,19 @@ define([
             var $container = $("#corr_scatterplot"),
             container = $container.get(0);
 
-            var divergentColorScale = ['#D7191C', '#FFFFBF', '#2B83BA']; //peach, yellow, green
+            var divergentColorScale = ['#ee0404', '#FFFF11', '#2B33CC']; //peach, yellow, green
+            var color_data = _.pluck(__.data_array,__.color_property);
+            var domain = d3.extent(color_data);
+            domain.splice(1,0,d3.median(color_data));
 
-            linearScale = d3.scale.linear().domain([.7,0,-.7]).range(divergentColorScale),
+            linearScale = d3.scale.linear().domain(domain).range(divergentColorScale),
             colorDataFn = function colorPoint(data) {
                 return linearScale(data[__.color_property]);
             }
 
             $container.html("");
-
-            var corr_sp =  new vq.ScatterPlot();
+            var sp = { version: '0.0.1'};
+            sp.corr_sp =  new vq.ScatterPlot();
 
             var plot_data = {
                 DATATYPE : "vq.models.ScatterPlotData",
@@ -69,7 +72,48 @@ define([
 
             $.extend(plot_data.CONTENTS, __);
 
-            corr_sp.draw(plot_data);
-            return corr_sp;
+            sp.corr_sp.draw(plot_data);
+
+            sp.legend = function(selection) {
+                var domain = linearScale.domain(),
+                range = linearScale.range(),
+                width = $(selection).width() || 200,
+                height = $(selection).height() || 20,
+                bar_scale = d3.scale.linear().domain(domain).range([0,width/2,width]),
+                $canvas = $('<canvas>').attr({id:'correlation_legend',width:width, height:height});
+
+                var canvas = $canvas.get(0);
+                $(selection).empty();
+                
+                $(selection).append(canvas);  
+                
+                var ctx = canvas.getContext("2d");
+                ctx.clearRect(0, 0, width, height);
+
+                var grad = ctx.createLinearGradient(0,0, width,0);
+                grad.addColorStop(0,range[0]);
+                grad.addColorStop(0.5,range[1]);
+                grad.addColorStop(1,range[2]);
+
+                ctx.rect(0, 0, width, height);
+                ctx.fillStyle=grad;
+                ctx.fill();
+
+                var color_axis = d3.svg.axis()
+                                    .orient('bottom')
+                                    .ticks(5)
+                                    .tickSize(6, 0, 6)
+                                    .scale(bar_scale);
+
+                d3.select(selection).append('svg')
+                    .attr('width',width)
+                    .attr('height',30)
+                .append('g')
+                    .attr('class','x axis')
+                    .call(color_axis);
+
+            };
+
+            return sp;
         };
 });
